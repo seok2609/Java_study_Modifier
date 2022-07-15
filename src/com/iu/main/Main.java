daab5ee862d6cbf9b096ca9faaa3a1ca55027c7e
seok2609 <hjs120926@naver.com>
2022-07-15 16:06:10 +0900

    1

--- src/com/iu/main/Main.java
+++ src/com/iu/main/Main.java
@@ -1,13 +1,26 @@
 package com.iu.main;
 
 import com.iu.terran.Marine;
+import com.iu.terran.Scv;
+import com.iu.unit.Unit;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Marine m1 = new Marine();
-		m1.setHp(30);
-		m1.info();
-		m1.move();
+		Scv sv1 = new Scv();
+		
+		//Marine is a Unit
+		//Scv    is a Unit
+		
+		Unit unit = m1;
+		System.out.println(unit.getHp());
+		System.out.println(unit.getColor());
+		System.out.println(unit.getName());
+	
+		
+		m1 = (Marine)unit;
+		System.out.println(m1.getWeapon());
+		
 	}
 }
